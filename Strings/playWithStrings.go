7cd040db761ad3f31d3816cfe3a10d76a5eb3944
lostinopensrc <lostinopensrc@gmail.com>
2023-02-20 13:19:31 +0530

    added other string functions

--- Strings/playWithStrings.go
+++ Strings/playWithStrings.go
@@ -1,4 +1,11 @@
-// This code demonstrates playing wit strings libary and its different functions :
+/*
+	This code demonstrates playing wit strings libary and its different functions like
+replace
+trimspace
+length
+UpperCase
+*/
+
 package main
 
 import (
@@ -18,11 +25,29 @@ func replaceme(myvalue string) {
 
 }
 
+func trimspace(myvalue string) {
+	trimValue := strings.TrimSpace(myvalue)
+	pl("Trimmed value :", trimValue)
+}
+
+func getlen(myvalue string) {
+	length := len(myvalue)
+	pl("Length :", length)
+}
+
+func toupper(myvalue string) {
+	value := strings.ToUpper(myvalue)
+	pl("UPPERCASE IS :", value)
+}
+
 func main() {
 	pl("Enter the string to play with")
 	reader := bufio.NewReader(os.Stdin)
 	mystring, err := reader.ReadString('\n')
 	if err == nil {
 		replaceme(mystring)
+		trimspace(mystring)
+		getlen(mystring)
+		toupper(mystring)
 	}
 }
