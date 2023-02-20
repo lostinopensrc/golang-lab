9412ededb02f2c736078a8f0c0abfb6d41894ce6
lostinopensrc <lostinopensrc@gmail.com>
2023-02-20 12:37:30 +0530

    added playWithString go file

--- Strings/playWithStrings.go
+++ Strings/playWithStrings.go
@@ -1,1 +1,28 @@
+// This code demonstrates playing wit strings libary and its different functions :
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var pl = fmt.Println
+
+func replaceme(myvalue string) {
+	replacer := strings.NewReplacer(myvalue, "Gaurav Pande")
+	replacedValue := replacer.Replace(myvalue)
+	pl("Original value:", myvalue)
+	pl("Replaced value :", replacedValue)
+
+}
+
+func main() {
+	pl("Enter the string to play with")
+	reader := bufio.NewReader(os.Stdin)
+	mystring, err := reader.ReadString('\n')
+	if err == nil {
+		replaceme(mystring)
+	}
+}
